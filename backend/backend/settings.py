009b1b38eb0bb569bbb21699050ca5921d584957
saberbenhamda <saberbenhamda0@gmail.com>
2024-11-01 17:58:00 +0100

    added two options for s3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -239,5 +239,7 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
